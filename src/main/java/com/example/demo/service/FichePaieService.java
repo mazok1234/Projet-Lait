27ac807a31b010d757dc9ca60a6bd3b86527dc86
Noah Andria <noahandriamanana41@.com>
2026-06-18 21:18:35 +0300

    listing and filtering functionnality working

--- src/main/java/com/example/demo/service/FichePaieService.java
+++ src/main/java/com/example/demo/service/FichePaieService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 import com.example.demo.entity.FichePaie;
+import com.example.demo.entity.Employe;
 import com.example.demo.repository.FichePaieRepository;
 
 
@@ -18,6 +19,10 @@ public class FichePaieService {
 
     public List<FichePaie> getAllFichePaie(){
         return fichePaieRepository.findAll();
+    }
+
+    public List<FichePaie> findByMoisAndAnneeAndEmploye(Integer mois, Integer annee, Integer idEmploye){
+        return fichePaieRepository.findByMoisAndAnneeAndEmploye(mois, annee, idEmploye);
     }
 
 }
